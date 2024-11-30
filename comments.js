24561492caa4ff449ab2d1231c30807d70454089
Elliot Abrahamsson <112617656+elliotabrahamsson@users.noreply.github.com>
2024-11-30 20:42:53 +0000

    copilot third commit

--- comments.js
+++ comments.js
@@ -1,36 +1,8 @@
 // create web server
-// create a route for comments
-// create a route for comments/new
-// create a route for comments/:id
-// create a route for comments/:id/edit
-// create a route for comments/:id/delete
-// create a route for comments/:id/like
-
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.send('Comments Page');
-});
-
-router.get('/new', (req, res) => {
-  res.send('Create a new comment');
-});
-
-router.get('/:id', (req, res) => {
-  res.send('View a comment');
-});
-
-router.get('/:id/edit', (req, res) => {
-  res.send('Edit a comment');
-});
-
-router.get('/:id/delete', (req, res) => {
-  res.send('Delete a comment');
-});
-
-router.get('/:id/like', (req, res) => {
-  res.send('Like a comment');
-});
-
-module.exports = router;
+var express = require('express');
+var app = express();
+// add body parser middleware
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+//
